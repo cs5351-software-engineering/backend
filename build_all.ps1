ab5727c37a94079d80933c9d312a32b3c72166bc
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-10-05 16:29:49 +0800

    feat: script for build all

---
+++ build_all.ps1
@@ -0,0 +1,24 @@
+# Build develop image
+Write-Host "Building backend develop image..."
+Set-Location .\backend
+& .\build.develop.ps1
+
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "Develop image built successfully." -ForegroundColor Green
+} else {
+    Write-Host "Failed to build develop image." -ForegroundColor Red
+    exit 1
+}
+
+# Build release image
+Write-Host "`nBuilding backend release image..."
+& .\build.release.ps1
+
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "Release image built successfully." -ForegroundColor Green
+} else {
+    Write-Host "Failed to build release image." -ForegroundColor Red
+    exit 1
+}
+
+Write-Host "`nBoth images have been built successfully." -ForegroundColor Green
